b0fad44077e08449cf720985358bf1f07a799235
ALEJ4NDRO29 <josealejandro.r.29@gmail.com>
2019-10-11 17:16:48 +0200

    User.current...

--- frontend/src/js/components/show-authed.directive.js
+++ frontend/src/js/components/show-authed.directive.js
@@ -1,29 +1,46 @@
-function ShowAuthed(User) {
+function ShowAuthed(User, Login) {
   'ngInject';
 
   return {
     restrict: 'A',
-    link: function(scope, element, attrs) {
+    link: function (scope, element, attrs) {
       scope.User = User;
+      scope.Login = Login;
 
-      scope.$watch('User.current', function(val) {
-          // If user detected
-          if (val) {
-            if (attrs.showAuthed === 'true') {
-              element.css({ display: 'inherit'})
-            } else {
-              element.css({ display: 'none'})
-            }
-
-          // no user detected
+      scope.$watch('Login.currentUser', function (val) {
+        if (val) {
+          if (attrs.showAuthed === 'true') {
+            element.css({ display: 'inherit' })
           } else {
-            if (attrs.showAuthed === 'true') {
-              element.css({ display: 'none'})
-            } else {
-              element.css({ display: 'inherit'})
-            }
+            element.css({ display: 'none' })
           }
+        } else {
+          if (attrs.showAuthed === 'true') {
+            element.css({ display: 'none' })
+          } else {
+            element.css({ display: 'inherit' })
+          }
+        }
       });
+
+      // scope.$watch('User.current', function (val) {
+      //   // If user detected
+      //   if (val) {
+      //     if (attrs.showAuthed === 'true') {
+      //       element.css({ display: 'inherit' })
+      //     } else {
+      //       element.css({ display: 'none' })
+      //     }
+
+      //     // no user detected
+      //   } else {
+      //     if (attrs.showAuthed === 'true') {
+      //       element.css({ display: 'none' })
+      //     } else {
+      //       element.css({ display: 'inherit' })
+      //     }
+      //   }
+      // });
 
     }
   };
